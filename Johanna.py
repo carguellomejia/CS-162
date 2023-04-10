3eec3706a7aa01bea7b644f55d347efa647aa711
carguellomejia <130280199+carguellomejia@users.noreply.github.com>
2023-04-09 21:11:09 -0700

    Update Johanna.py

--- Johanna.py
+++ Johanna.py
@@ -1,11 +1,25 @@
+# will take 2 parameters - length and width.
 def rect_area(length, width):
     area = length * width
     return area
+# Write a second function named "rect_surface_area" that calls the first function -
+# "rect_area" - three times to compute the surface area of a rectangular solid.  This
+# function will take three parameters - length, width and height.
+def rect_surface_area(length, width, height):
+    sa = 2 * (rect_area(length, width) + rect_area(width, height) + rect_area(length, height))
+    print('Length =', length, 'Width =', width, 'Height = ', height)
+    return sa
+length1 = float(input('Enter length here: '))
+width1 = float(input('Enter width here: '))
+height1 = float(input('Enter height here: '))
+surface_area = rect_surface_area(length1, width1, height1)
+print('Total surface area =', surface_area)
 
-length = float(input('Enter the length of the rectangle: '))
-width = float(input('Enter the width of the rectangle: '))
+# The main program will call the "rect_surface_area" with three parameters length, width
+# and height.
 
-area = rect_area(length, width)
+# The user input to the program will be three integers - length, width and height of a
+# rectangular solid.
 
-print("Length = ", length, "Width = ", width)
-print("Area =", area)
+# The program will print the length, width and height as input and the surface area of
+# the entire rectangular solid.
