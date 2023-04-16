9d3b6fd50af34c6f4c8693344bf148f04f9aa5c4
carguellomejia <130280199+carguellomejia@users.noreply.github.com>
2023-04-16 15:24:04 -0700

    Update Picechartgraphic.py

--- Picechartgraphic.py
+++ Picechartgraphic.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plot
 # set up your lists
-numlist = [8, 6, 5, 3]
-namelist = ['freshmen', 'sophomores', 'juniors', 'seniors']
+numlist = [7, 6, 5, 1]
+namelist = ['Dinosaurs', 'humans', 'reptiles', 'plants']
 colorlist = ['red', 'green', 'pink', 'yellow' ]
 explodelist = [0.1, 0.0, 0.0, 0.0]
 # make the pie chart
 plot.pie(numlist, labels=namelist, autopct='%.2f%%', colors=colorlist,
-    	explode = explodelist, startangle = 90)
+    	explode = explodelist, startangle = 95)
 plot.axis('equal')
 plot.savefig('piechart.png')
+
